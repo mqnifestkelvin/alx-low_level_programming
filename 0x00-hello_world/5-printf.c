d0ba574c70e835f4f932c7aec9551e3debdbeb50
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-08 11:32:15 -0700

    A C program that prints with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main is the body of the code
+ *
+ * Return: Returns an integer value
+*/
+int main(void)
+{
+	char a[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", a);
+	return (0);
+}
