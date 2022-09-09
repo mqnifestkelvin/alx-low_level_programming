7fff2b0f242e01835511afeda2c54a551a84edea
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-09 16:03:11 -0700

    A program that prints all the possible combination of a three digit number.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This porgram prints all possible combinations
+ * of a 3 digit number without repition
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for  (c = 0; c <= 9; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if (a != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
