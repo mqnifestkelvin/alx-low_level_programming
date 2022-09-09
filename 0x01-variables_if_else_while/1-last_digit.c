48218754a1c58829e7924e266d6f76232e4feb09
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-09 05:57:29 -0700

    The program prints the last digit of a number and assumes whether its in the above 5 range or below 6 range or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if ((n % 10 < 6) && (n != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not \n0", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
