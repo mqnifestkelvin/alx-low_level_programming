49e0eb7fd313f1d10c45a27bb2ecc9f8b8719d9a
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-09 13:56:21 -0700

    A program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,12 @@ int main(void)
 	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
-		putchar(',');
-		putchar(' ');
+		
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
