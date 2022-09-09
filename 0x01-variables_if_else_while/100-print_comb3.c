7376c7e2addcdfc33732c4bd3347ee3fd14b53ad
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-09 15:26:15 -0700

    A program that prints all possible combination of all two digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,13 +18,13 @@ int main(void)
 				putchar(a + '0');
 				putchar(b + '0');
 
-				if (a != 8 || (a == 8 && b != 9) )
+				if (a != 8 || (a == 8 && b != 9))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		} 
+		}
 	}
 	putchar('\n');
 	return (0);
