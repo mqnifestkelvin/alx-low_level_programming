59fb4fc52b6cfbf819f767af8a503b7ee722379c
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-09 15:20:48 -0700

    A program that prints all possible combination of all two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints all possible combinations
+ * of a two digit number.
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (a < b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+
+				if (a != 8 || (a == 8 && b != 9) )
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		} 
+	}
+	putchar('\n');
+	return (0);
+}
