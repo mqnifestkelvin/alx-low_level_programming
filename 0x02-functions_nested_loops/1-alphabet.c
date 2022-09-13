5456d1c872397c3b91b21377623d61a01f40afc4
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-13 12:27:23 -0700

    A function that printd the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: A function that prints the alphabet::
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; aphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
