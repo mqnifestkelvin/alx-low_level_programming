85bb9a59c0a49e3029a444319d8f21b71a8e0541
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-13 15:06:43 -0700

    This function checks for alphabetic characters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * _isalpha -
+ * _isalpha - Code entry.
  *
- * Description:
+ * Description: A function that checks for alphabetic character. 
  *
- * Return:
+ * Return: 0
  */
 int _isalpha(int c)
 {
 	char cap_alphabet, icap_alphabet;
 
-	for (cap_alphabet = 'a'; cap_alphabet = 'z'; cap_alphabet++ )
+	for (cap_alphabet = 'a'; cap_alphabet <= 'z'; cap_alphabet++)
 	{
-		for (icap_alphabet = 'A'; icap_alphabet = 'Z'; icap_alphabet++)
+		for (icap_alphabet = 'A'; icap_alphabet <= 'Z'; icap_alphabet++)
 		{
 			if ((cap_alphabet == c) && (icap_alphabet == c))
 			{
-				return (0);
+				return (1);
 			}
 		}
 	}
+	return (0);
 }
