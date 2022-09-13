8c6ac27d41d1350212468f82828bdbc68a9099f2
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-13 13:43:54 -0700

    This program identifies lower cases

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * int _islower(int c)
+ *
+ * Description: This program checks for lowercase character.
+ *
+ * Return: 0
+ */
+int _islower(int c);
+{
+	int i = 'a';
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refer in c*/
+		if (c == 1)
+		{
+			return (1);
+		}
+
+	}
+	return (0);
+}
