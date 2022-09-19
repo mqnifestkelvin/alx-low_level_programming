7c7ee151a01cd7e1e736e12433e87231202e28a6
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 15:02:57 -0700

    This function prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - A function that prints a string in reverse.
+ *
+ * @s: This is the string value.
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
