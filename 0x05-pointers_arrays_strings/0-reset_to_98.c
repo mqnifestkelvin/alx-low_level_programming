65f722bb5aca81fff6b22c3ec550695b79668878
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 08:40:12 -0700

    This code turns the value of n to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * main - checks the code
+ * reset_to_98 - This function turns any value of n to 98
+ *
+ * @n: This is an integer pointer
  *
  * Return: 0
  *
  */
 void reset_to_98(int *n)
 {
-	n = 98;
+	*n = 98;
 }
