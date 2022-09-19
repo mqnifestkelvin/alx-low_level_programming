465b9e09a2938bfd5c34b3d2947293e4ba30648c
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 12:00:05 -0700

    A function that prints the length of a value

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - This function checks the length of a string.
+ *
+ * @s: This accepts a string value 
+ *
+ * Return: 0
+ *
+ */
+int _strlen(char *s)
+{
+	char string[] = s;
+	int i,
+
+	for(i = 0; c[i] != '\0'; i++);
+	return (i);
+}
