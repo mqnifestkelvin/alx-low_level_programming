e99087e6d60d4fa9c72d170002d54414e01b932f
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 17:47:49 -0700

    This program makes a copy of a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
 /**
- * _strcpy - This function copies strings pointed to by src.
- *
- * @dest: char type.
- *
- * @src: char type.
- *
- * Return: pointer to dest.
- *
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -15,7 +14,6 @@ char *_strcpy(char *dest, char *src)
 	int i = -1;
 
 	do {
-
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
