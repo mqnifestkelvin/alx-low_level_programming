afb4b66602c04d5f2624e41aeac4e9d818781607
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 17:50:33 -0700

    This program makes a copy of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,8 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int atoi(char *s);
+
+
 #endif
