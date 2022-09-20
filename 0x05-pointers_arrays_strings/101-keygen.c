b0685c604f286d32e02560e667ce55e424b146dc
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-19 18:00:26 -0700

    A program that generates random passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
 /**
-* main - generates keygen.
-* Return: 0 Always.
-*/
+ * main - print password.
+ *
+ * Return: 0.
+ */
+
 int main(void)
 {
-int r = 0, c = 0;
-time_t t;
+	int ascii = 2772, i = 0, j, random;
+	char password[100];
+	time_t t;
 
-srand((unsigned int) time(&t));
-while (c < 2772)
-{
-r = rand() % 128;
-if ((c + r) > 2772)
-break;
-c = c + r;
-printf("%c", r);
-}
-printf("%c\n", (2772 - c));
-return (0);
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+	
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
 }
