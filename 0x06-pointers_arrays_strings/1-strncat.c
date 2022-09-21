aa62e2c3fd114f4ec213a969f027d96692501df5
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-21 14:41:01 -0700

    This funtion concatenates two strings together

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@
 char *_strncat(char dest, char src, int n)
 {
 	int dest_len, i;
-	for (dest_len = 0; dets[dest_len] != '\0'; dest_len++)
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 		;
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
