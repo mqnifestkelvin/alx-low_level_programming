ce0cdd9cf4037279efac9c175a30e0edfe0109af
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-22 01:38:58 -0700

    This array reverses a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - This function reverses an array.
+ *
+ * @a: An array of integers.
+ *
+ * @n: The number of elements to swap.
+ *
+ * Return: 0
+ */
+void reverse_array(int *a, int n);
+{
+	int i, half;
+
+	for (half = n / 2; half > 0; half--, i++)
+	{
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
+	}
+}
