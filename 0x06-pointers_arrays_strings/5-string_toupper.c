cf976827f610c571e8660540f41fa6525429f9ee
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-22 02:11:05 -0700

    This function converts uppercase strings to lowercase strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *string_toupper - This function capitalizes a string.
+ *
+ * @s: This is the pointer to the string.
+ *
+ * Return: Capitalized string.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++
+	}
+	return (s);
+}
