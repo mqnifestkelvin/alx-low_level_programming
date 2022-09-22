fc19bb76879ec0f4f671e25cf335b08225dc995c
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-22 10:14:29 -0700

    This function prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - This function prints an integer.
+ *
+ * @n: integer.
+ *
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+	int div = 1, i, resp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -i
+	}
+	for (i = 0; n / div > 9; i++, div *= 10)
+		;
+	for (; div >= 1; n %= div, div /= 10)
+	{
+		resp = n / div;
+		_putchar('0' + resp);
+	}
+}
