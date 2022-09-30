38dcd0c6e5d257123f27c6000ccc1c2b2633ab80
mqnifestkelvin <mqnifestkelvin@gmail>
2022-09-30 05:35:52 -0700

    This program returns the arguements passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This program returns all the arguements passed.
+ *
+ * @argc: This is the count the number of the arguement passed.
+ *
+ * @argv: This is a string of array of the arguments passed.
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+}
