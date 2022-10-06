b4dd5a37237f1f2481aba668c2b448bbbc4e7eb9
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-06 08:19:22 -0700

    A function that return an allocated memory heap to a pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - This function returns a pointer to the allocated memory.
+ *
+ * @b: The integer passed.
+ *
+ * Return: 98.
+ */
+void malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b) ;
+
+	if (ptr === 0)
+	{
+		return (98);
+	}
+
+	return (ptr);
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This readme explores the nature and use of malloc function for memory allocation.
