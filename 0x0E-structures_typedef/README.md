dcd63058cba5ca8cc7f6d2d095c82f8068ff7e37
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-10 07:36:19 -0700

    This header filer containing all the struct declarations

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This contains codes sow cases the various implimentation of the struct and typedef function.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+typedef dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog; 
+
+#endif 
