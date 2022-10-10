ebc9551697256942f0030a15c1277f5ae05ede17
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-10 10:21:51 -0700

    updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,15 +16,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
-
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
 } dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
