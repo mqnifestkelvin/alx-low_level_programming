cbfe14f1872b1fa5ffb5b539322fce0b48df75be
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-10 08:54:18 -0700

    Ths function initializes a variabe of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,12 +16,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != 0)
 	{
-		dog Dog;
-		dog *ptr;
-		ptr = &Dog;
-
-		(*ptr).name = name;
-		(*ptr).age = age;
-		(*ptr).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
