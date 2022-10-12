3e13fc9b3a3f981999eadba7319034aeec6dc244
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-12 13:03:35 -0700

    This program searches for index in an array of integers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,12 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == 0 || cmp == 0)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
