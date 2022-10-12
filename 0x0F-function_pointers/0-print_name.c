ef1a8c97b8ea67a0757f7bb86dd1bd5cff56cba4
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-12 10:54:03 -0700

    A function that prints

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include " function_pointers.h"
+#include "function_pointers.h"
 
 /**
  * print_name - Prints name.
@@ -13,5 +13,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != 0 && f != 0)
+	{
 		f(name);
+	}
 }
