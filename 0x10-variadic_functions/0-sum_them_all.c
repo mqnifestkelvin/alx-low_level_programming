4195b6888579bd62b1ad7378ba5ba3f93e3872e5
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-13 09:10:01 -0700

    A function that adds an infinite number of arguements provided the length is given

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int i, sum = 0;
+
 	if (n == 0)
 		return (0);
-
-	va_list ap;
-	int i, sum = 0;
-	
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
-#include <stdarg>
+#include <stdarg.h>
+
+/**
+ * struct printer - A new struct type defining a pointer.
+ *
+ * @symbol: A symbol representing a data type.
+ *
+ * @print: A function poniter to a function that prints
+ * a data type similar to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
