2f33370f79223f7ef94ff9ab306cf88b403c6140
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-13 08:18:30 -0700

    A function that adds an infinite number of arguements provided the length is given

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	if (n == 0)
 		return (0);
+
 	va_list ap;
-
-	va_start(ap, n);
-
 	int i, sum = 0;
+	
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
