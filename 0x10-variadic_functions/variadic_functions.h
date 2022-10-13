498535d7dbfcd3c77f22f083a1b8f292318762f8
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-13 08:31:23 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
+#include <stdarg>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
