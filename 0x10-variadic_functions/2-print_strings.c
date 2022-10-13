1d71fcafd2368ea4ce75950507bf683f645eef99
mqnifestkelvin <mqnifestkelvin@gmail>
2022-10-13 13:02:59 -0700

    A function that prints any amount of strings passed

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - This function prints any amount of string passed
+ * as arguement.
+ *
+ * @separator: This any character passed to be used as a separator for
+ * each strings.
+ *
+ * @n: This is the number of arguements/strings passed.
+ *
+ * Return: Prints the String arguements passed.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *ptr;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(ap, char *);
+
+		if (ptr == 0)
+		{
+			printf("(nil)");
+		}
+
+		else
+		{
+
+			printf("%s", ptr);
+		}
+		
+		if (i !=(n - 1) && separator != 0)
+		{
+			printf("%s", separator); 
+		}
+	}
+	printf("\n");
+
+	va_end(ap);
+}
