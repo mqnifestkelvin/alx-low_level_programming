be3fada6b396bae2b1344385261b76bbc3d3715f
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-10-24 23:16:26 +0100

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,8 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes in a looped listint_t linked list.
+ * looped_listint_len - Counts the number of unique nodes in a
+ * looped listint_t linked list.
  *
  * @head: A pointer to the head of the listint_t to check.
  *
