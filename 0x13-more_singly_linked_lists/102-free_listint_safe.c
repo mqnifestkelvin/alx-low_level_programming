b936204e19db8204b91d047270b2c8b204336460
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-10-24 23:19:50 +0100

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,7 +4,7 @@ size_t looped_listint_count(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 /**
- * looped_listint_count - Counts the number of unique nodes in a 
+ * looped_listint_count - Counts the number of unique nodes in a
  * looped listint_t linked list.
  *
  * @head: A pointer to the head of the listint_t to check.
