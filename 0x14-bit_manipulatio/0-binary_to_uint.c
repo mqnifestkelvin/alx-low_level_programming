fb4b184ee8474f5ee207642b552bc2327fd4a3a1
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-10-27 22:41:58 +0100

    main.h file and a program that returns a binary number

--- 0x14-bit_manipulatio/0-binary_to_uint.c
+++ 0x14-bit_manipulatio/0-binary_to_uint.c
@@ -1,19 +1,27 @@
 #include "main.h"
-#include <string.h>
-unsigned int binary_to_uint(const cahr *b)
-{
-	int slen = strlen(string);
-	int total = 0;
-	int decval = 1;
-	int i;
 
-	for (i = (slen - 1); i >= 0; i--}
-			{
-				if (string[i] == '1')
-				{
-					total += decval;
-					decval *= 2;
-				}
-			}
-			return (total);
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ *
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ * Otherwise - the converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int len = 0;
+
+	if (b[len] == '\0')
+		return (0);
+
+	while ((b[len] == '0') || (b[len] == '1'))
+	{
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
+	}
+
+	return (num);
 }
