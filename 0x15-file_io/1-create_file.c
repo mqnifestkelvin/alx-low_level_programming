89dfa90a7d4088f87336d9a871516dde911a8c96
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-01 02:14:17 +0100

    create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,9 @@
 
 /**
  * create_file - Creates a file.
+ *
  * @filename: A pointer to the name of the file to create.
+ *
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
