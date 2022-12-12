6a8eca8ec64cfc090094341cdf735ebb474cdeaf
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-12 16:00:22 +0100

    dynamic

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - addition
+ * @a: first number
+ * @b: second number
+ * Return: sum of first number and second number
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - subtraction
+ * @a: first number
+ * @b: second number
+ * Return: difference of first number and second number
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+/**
+ * mul - multiplication
+ * @a: first number
+ * @b: second number
+ * Return: product of first number and second number
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * div - division
+ * @a: first number
+ * @b: second number
+ * Return: divison of first number and second number
+ */
+int div(int a, int b)
+{
+	return a / b;
+}
+
+/**
+ * mod - modulo
+ * @a: first number
+ * @b: second number
+ * Return: sum of first number and second number
+ */
+int mod(int a, int b)
+{
+	return a % b;
+}
