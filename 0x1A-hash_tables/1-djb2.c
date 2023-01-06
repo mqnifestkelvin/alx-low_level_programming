b685091aa35fb12105f1ede17b0157682a11a7c5
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-06 21:13:36 +0100

    a hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
+
 /**
- * hash_djb2 - The hashing function implementing the djb2
- * algorithm
- * @str: String used to generate hash values
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
  *
- * Return: hash value
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -13,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+
 	return (hash);
+}
